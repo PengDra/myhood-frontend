7c06228b76e31dfb649bf90e1a5308e1e5c62c94
Gn3p <Gn3p@DESKTOP-HQFFS0H>
2022-05-12 16:10:17 -0400

    Colores definidos

---
+++ android/app/src/main/kotlin/com/example/myhood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myhood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/src/utils/my_colors.dart
+++ lib/src/utils/my_colors.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static const Color primary = Color(0xFFFFC107);
+  static const Color primary = Color(0xFF86BBD8);
+  static const Color black = Color(0xff343434);
+  static const Color white = Color(0xFFFFECD1);
+  static const Color red = Color(0xFFF72C25);
+  static const Color purple = Color(0xFF613DC1);
 }
