62a4962b965d167f03a076ff4823e2f9b325cf21
Gn3p <Gn3p@DESKTOP-HQFFS0H>
2022-05-12 16:16:36 -0400

    IMG

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myhood/src/login/login_page.dart';
+import 'package:myhood/src/utils/my_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +23,7 @@ class _MyAppState extends State<MyApp> {
         'login':(BuildContext context)=>LoginPage(),
       },
       theme: ThemeData(
-        primaryColor: 
+        primaryColor: MyColors.primary,
       ),
       
     );
