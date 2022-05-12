5f8b5f6bdadb8d843f5970a11546871e5f6460d9
Gn3p <Gn3p@DESKTOP-HQFFS0H>
2022-05-12 18:20:52 -0400

    pantalla log in001

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myhood/src/utils/my_colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,12 +12,106 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              _imageBanner(),
+              _textFieldEmail(),
+              _textFieldPassword(),
+              _buttonLogin(),
+              _textDontHaveAccount()
+            ],
+          )),
+    );
+  }
 
-        
+  Widget _imageBanner() {
+    return Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.22, bottom: MediaQuery.of(context).size.height * 0.22),
+      child: Image.asset(
+        'assets/img/iconomyhood300x300.png',
+        width: 200,
+        height: 200,
+      ),
+    );
+  }
 
+  Widget _textFieldEmail() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacity,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child:  TextField(
+        decoration: InputDecoration(
+          hintStyle: TextStyle(color: MyColors.black),
+          hintText: 'Email',
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(Icons.email, color: MyColors.primary),
+        ),
+      ),
+    );
+  }
 
+  Widget _textFieldPassword() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      decoration: BoxDecoration(
+        color: MyColors.primaryOpacity,
+        borderRadius: BorderRadius.circular(30)
+      ),
+      child:  TextField(
+        decoration: InputDecoration(
+          hintStyle: TextStyle(color: MyColors.black),
+          hintText: 'Password',
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.all(15),
+          prefixIcon: Icon(Icons.lock, color: MyColors.primary),
+        ),
+      ),
+    );
+  }
 
-      ));
+  Widget _buttonLogin() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('Iniciar sesión'),
+        style: ElevatedButton.styleFrom(
+          primary: MyColors.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 15)
+
+        )
+          
+        ),
+    );
+  }
+
+  Widget _textDontHaveAccount() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Text('¿No tienes una cuenta?',
+            style: TextStyle(
+              color: MyColors.primary,
+            )),
+        SizedBox(
+          width: 10,
+        ),
+        Text('Registrate',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: MyColors.purple,
+            )),
+      ],
+    );
   }
 }
--- lib/src/utils/my_colors.dart
+++ lib/src/utils/my_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyColors {
   static const Color primary = Color(0xFF86BBD8);
+  static const Color primaryOpacity = Color.fromRGBO(134, 187, 216, 0.5);
   static const Color black = Color(0xff343434);
   static const Color white = Color(0xFFFFECD1);
   static const Color red = Color(0xFFF72C25);
